84cd85601b13210908d34b141625a1b244dc8903
Michael Douglas Bonfogo <59799217+MichaelBonfogo@users.noreply.github.com>
2021-04-05 13:09:28 -0300

    Update
    Update.

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,22 @@
-class CalcController {
-     
+class CalcController{
+    constructor(){
+        this._displayCalc = "0";
+        this._dataAtual;
+        }
+
+        get display(){
+            return this._displayCalc;
+        }
+
+        set display(valor){
+            this._displayCalc = valor;
+        }
+
+        get dataAtual(){
+            return this._dataAtual;
+        }
+
+        set dataAtual(valor){
+            this._dataAtual = valor;
+        }
 }
